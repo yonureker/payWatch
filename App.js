803f7d383ce39f99e459aedc4ebe71cc169d6648
yonureker <yonureker@gmail.com>
2019-10-12 22:12:58 -0700

    update login form

--- App.js
+++ App.js
@@ -8,21 +8,77 @@ firebase.initializeApp(firebaseConfig);
 export default class App extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      email: '',
+      password: ''
+    }
   }
 
   render() {
     return (
-      <View className="first" style={styles.first}></View>
+      <Container style={styles.container}>
+        <Form>
+          <Item floatingLabel>
+            <Label>Email</Label>
+            <Input
+              autoCorrect={false}
+              autoCapitalize="none"
+              onChangeText={(email) => this.setState({ email })}
+            />
+
+          </Item>
+
+          <Item floatingLabel>
+            <Label>Password</Label>
+            <Input
+              secureTextEntry={true}
+              autoCorrect={false}
+              autoCapitalize="none"
+              onChangeText={(password) => this.setState({ password })}
+            />
+          </Item>
+
+          <Button style={{ marginTop: 10 }}
+            full
+            rounded
+            success
+            onPress={() => this.loginUser(this.state.email, this.state.password)}
+          >
+            <Text style={{ color: 'white' }}> Login</Text>
+          </Button>
+
+          <Button style={{ marginTop: 10 }}
+            full
+            rounded
+            primary
+            onPress={() => this.signUpUser(this.state.email, this.state.password)}
+          >
+            <Text style={{ color: 'white' }}> Sign Up</Text>
+          </Button>
+
+          <Button style={{ marginTop: 10 }}
+            full
+            rounded
+            primary
+            onPress={() => this.loginWithFacebook()}
+          >
+            <Text style={{ color: 'white' }}> Login With Facebook</Text>
+          </Button>
+
+
+
+        </Form>
+      </Container>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  first: {
+  container: {
     flex: 1,
     backgroundColor: '#fff',
     justifyContent: 'center',
-    alignItems: 'center',
-    padding: 5
+    padding: 10
   },
 });
